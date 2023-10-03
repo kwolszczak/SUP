a4d65325a54a4cf7b1718d6ce220e7f632197a64
Kamil Wolszczak <kwolszczak@sii.pl>
2023-10-04 00:33:19 +0200

    homework java 1.1, 1.2

--- src/main/java/pl/kwolszczak/java1_2/Main.java
+++ src/main/java/pl/kwolszczak/java1_2/Main.java
@@ -1,8 +1,10 @@
 package pl.kwolszczak.java1_2;
 
 import java.util.Random;
+
 public class Main {
     private static final Random random = new Random();
+
     public static void main(String[] args) {
         System.out.println(tokenGenerator(3));
         System.out.println(tokenGenerator(18));
@@ -10,14 +12,14 @@ public class Main {
     }
 
     public static String tokenGenerator(int tokenSize) {
-        char tokenChar;
         StringBuilder token = new StringBuilder(15);
+        int asciiPosition;
+        char tokenChar;
 
         return switch (tokenSize) {
             case 5, 10, 15 -> {
                 for (int i = 0; i < tokenSize; i++) {
-                    int asciiPosition = random.nextInt(33, 127);
-
+                    asciiPosition = random.nextInt(33, 127);
                     tokenChar = (char) asciiPosition;
                     token.append(tokenChar);
                 }
