7e2e746624cdf3a0bbc894af8fe42210eff70979
Kamil Wolszczak <kwolszczak@sii.pl>
2023-10-04 14:05:27 +0200

    test git branch

---
+++ src/main/java/pl/kwolszczak/test.java
@@ -0,0 +1,14 @@
+package pl.kwolszczak;
+
+public class test {
+    private String string;
+    private int anInt;
+
+
+    public static void main(String[] args) {
+
+        String myString = """
+                dsfsd kk k """;
+
+    }
+}
