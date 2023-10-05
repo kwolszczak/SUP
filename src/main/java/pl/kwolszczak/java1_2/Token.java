3de968ed3fc7d3902328853fdc22edc7990d9362
Kamil Wolszczak <kwolszczak@sii.pl>
2023-10-05 08:25:35 +0200

    exclude method Token from main class

---
+++ src/main/java/pl/kwolszczak/java1_2/Token.java
@@ -0,0 +1,24 @@
+package pl.kwolszczak.java1_2;
+
+import java.util.Random;
+
+public class Token {
+    private static final Random random = new Random();
+    public static String tokenGenerator(int tokenSize) {
+        StringBuilder token = new StringBuilder(15);
+        int asciiPosition;
+        char tokenChar;
+
+        return switch (tokenSize) {
+            case 5, 10, 15 -> {
+                for (int i = 0; i < tokenSize; i++) {
+                    asciiPosition = random.nextInt(33, 127);
+                    tokenChar = (char) asciiPosition;
+                    token.append(tokenChar);
+                }
+                yield token.toString();
+            }
+            default -> "Incorrect token Size parameter!";
+        };
+    }
+}
--- src/main/java/pl/kwolszczak/test.java
+++ src/main/java/pl/kwolszczak/test.java
@@ -4,13 +4,23 @@ public class test {
     private String string;
     private int anInt;
 
+    public test(String color){
+
+    }
+
 
     public static void main(String[] args) {
+
 
         String myString = """
                 dsfsd kk k """;
 
         String myss="dsdf";
+        System.out.println(myss);
+
+        String[] mys = new String[3];
+        System.out.println(mys);
+        System.out.println(mys.hashCode());
 
 
     }
