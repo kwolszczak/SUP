f86fcc54778f799a90f320579f1238bbfd3d9783
Kamil Wolszczak <kwolszczak@sii.pl>
2023-11-07 18:34:41 +0100

    selenium exercise 6.1

--- src/test/java/pl/kwolszczak/selenium6_1/pages/others/HighSitePageTest.java
+++ src/test/java/pl/kwolszczak/selenium6_1/pages/others/HighSitePageTest.java
@@ -1,73 +1,40 @@
 package pl.kwolszczak.selenium6_1.pages.others;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import pl.kwolszczak.selenium6_1.BaseTest;
-import org.apache.commons.io.FileUtils;
 
-import java.io.File;
 import java.io.IOException;
 
+import static pl.kwolszczak.selenium6_1.util.SeleniumUtil.*;
+
 public class HighSitePageTest extends BaseTest {
+    String url = "http://automation-practice.emilos.pl/high-site.php";
 
     @Test
     //@RepeatedTest(10)
     @DisplayName("High Test Scroll")
     void highPageTest_scroll_whenUseScrollOption() throws IOException {
-        driver.get("http://automation-practice.emilos.pl/high-site.php");
-        boolean isVisible = false;
-        Actions actions = new Actions(driver);
-        WebElement visibleBtn;
 
+        driver.get(url);
+        boolean result = scrollAndCheckVisibility(driver,800);
+        takeScreenShoot(driver);
 
-
-
-        while (!isVisible) {
-            actions.scrollByAmount(0, 100).build().perform();
-            try {
-                visibleBtn = driver.findElement(By.cssSelector("input#scroll-button"));
-                isVisible = visibleBtn.isDisplayed();
-                System.out.println("founded");
-            } catch (Exception e) {
-                System.out.println("not visible yet");
-            }
-        }
-
-        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String path = "target/screenshot.png";
-        FileUtils.copyFile(screenshot, new File(path));
-
+        Assertions.assertThat(result).isTrue();
     }
+
     @Test
     //@RepeatedTest(10)
     @DisplayName("High Test JSExecutor")
     void highPageTest_scroll_whenUseJSExecutor() throws IOException {
-        driver.get("http://automation-practice.emilos.pl/high-site.php");
-        boolean isVisible = false;
 
-        WebElement visibleBtn;
-        JavascriptExecutor jsExec = (JavascriptExecutor)driver ;
+        driver.get(url);
+        boolean result = scrollAndCheckVisibilityJSExecutor(driver, 100);
+        takeScreenShoot(driver);
 
-
-
-        while (!isVisible) {
-            jsExec.executeScript("window.scrollBy(0,100)");
-            try {
-                visibleBtn = driver.findElement(By.cssSelector("input#scroll-button"));
-                isVisible = visibleBtn.isDisplayed();
-                System.out.println("founded");
-            } catch (Exception e) {
-                System.out.println("not visible yet");
-            }
-        }
-
-
-        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String path = "target/screenshot.png";
-        FileUtils.copyFile(screenshot, new File(path));
-
+        Assertions.assertThat(result).isTrue();
     }
 }
--- src/test/java/pl/kwolszczak/selenium6_1/util/SeleniumUtil.java
+++ src/test/java/pl/kwolszczak/selenium6_1/util/SeleniumUtil.java
@@ -1,9 +1,14 @@
 package pl.kwolszczak.selenium6_1.util;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
@@ -12,6 +17,7 @@ import java.util.Set;
 public class SeleniumUtil {
 
     private static Random random = new Random();
+    private static Logger log = LoggerFactory.getLogger(SeleniumUtil.class);
 
     public static void fill(WebElement webElement, String text){
         webElement.clear();
@@ -41,12 +47,59 @@ public class SeleniumUtil {
     }
 
     public static String switchToSecondWindow(WebDriver driver){
+        log.info("Switching to second window");
         Set<String> windowHandles = driver.getWindowHandles();
         Iterator<String> iterator = windowHandles.iterator();
         String firstWindowHandle = iterator.next();
         String secondWindowHandle = iterator.next();
+        log.debug("first window handle {} second window handle: {}",firstWindowHandle,secondWindowHandle);
         driver.switchTo().window(secondWindowHandle);
 
         return firstWindowHandle;
+    }
+
+    public static boolean scrollAndCheckVisibility(WebDriver driver,int pixels) {
+
+        Actions actions = new Actions(driver);
+        boolean isVisible = false;
+        while (!isVisible) {
+            actions.scrollByAmount(0, pixels).build().perform();
+            try {
+                WebElement button = driver.findElement(By.cssSelector("input#scroll-button"));
+                isVisible = button.isDisplayed();
+                log.info("Founded searched button {}",button.getText());
+                return true;
+            } catch (Exception e) {
+                log.debug("Not Founded yet button");
+            }
+        }
+        return false;
+    }
+
+    public static boolean scrollAndCheckVisibilityJSExecutor(WebDriver driver,int pixels) {
+
+        JavascriptExecutor jsExec = (JavascriptExecutor) driver;
+        boolean isVisible = false;
+        while (!isVisible) {
+            jsExec.executeScript("window.scrollBy(0,"+pixels+")");
+            try {
+                WebElement button = driver.findElement(By.cssSelector("input#scroll-button"));
+                isVisible = button.isDisplayed();
+                log.info("Founded searched button {}",button.getText());
+                return true;
+            } catch (Exception e) {
+                log.debug("Not Founded yet button");
+            }
+        }
+        return false;
+    }
+
+    public static void takeScreenShoot(WebDriver driver) throws IOException {
+
+        String fileName = "screenshot.png";
+        log.info("Take a screenshot {}",fileName);
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = "target/"+fileName;
+        FileUtils.copyFile(screenshot, new File(path));
     }
 }
