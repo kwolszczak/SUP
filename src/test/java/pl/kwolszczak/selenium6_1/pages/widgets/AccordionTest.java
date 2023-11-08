01851bf2ab9900f8aa68a0998b8a646cdd52f120
Kamil Wolszczak <kwolszczak@sii.pl>
2023-11-08 22:49:16 +0100

    selenium exercise 6.1

--- src/test/java/pl/kwolszczak/selenium6_1/pages/widgets/AccordionTest.java
+++ src/test/java/pl/kwolszczak/selenium6_1/pages/widgets/AccordionTest.java
@@ -3,6 +3,7 @@ package pl.kwolszczak.selenium6_1.pages.widgets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,28 +21,24 @@ class AccordionTest extends BaseTest {
     void accordionPageTest_simpleTest() {
 
         driver.get(url);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        var wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        driver.findElement(By.cssSelector("#ui-id-5")).click();
+        for (int i = 1; i <= 7; i += 2) {
+            var selector = "#ui-id-" + i;
+            var childSelector = "//h3[@id='ui-id-" + i + "']/following-sibling::div[1]/*";
 
-     wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(By.xpath("//h3[@id='ui-id-5']/following-sibling::div[1]/*"))));
+            driver.findElement(By.cssSelector(selector)).click();
+            wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(By.xpath(childSelector))));
+            printData(childSelector, driver);
+        }
 
-        driver.findElements(By.xpath("//h3[@id='ui-id-5']/following-sibling::div[1]/*"))
+    }
+
+    private static void printData(String selector, WebDriver driver) {
+        driver.findElements(By.xpath(selector))
                 .stream()
                 .map(WebElement::getText)
                 .filter(s -> !s.isEmpty())
                 .forEach(System.out::println);
-
-
-        driver.findElement(By.cssSelector("#ui-id-7")).click();
-      wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(By.xpath("//h3[@id='ui-id-7']/following-sibling::div[1]/*"))));
-
-        driver.findElements(By.xpath("//h3[@id='ui-id-7']/following-sibling::div[1]/*"))
-                .stream()
-                .map(WebElement::getText)
-                .filter(s -> !s.isEmpty())
-                .forEach(System.out::println);
-
-
     }
 }
